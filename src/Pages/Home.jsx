f311fc8e6ae16c8fed59a4a64e162e7f3e2dff24
prithvikings <prithvi07raj07@gmail.com>
2025-01-28 13:23:17 +0530

    Add axios for API calls and implement login functionality in Login component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import Login from "./Login";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
 const Login = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const handlelogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:3000/login", {
+        email: Email, // lowercase 'email'
+  password: Password, // lowercase 'password'
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex items-center justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <div>
+            <input
+              type="text"
+              value={Email}
+              placeholder="Email"
+              className="input input-bordered outline-none my-4"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              value={Password}
+              placeholder="Password"
+              className="input input-bordered outline-none  my-4"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handlelogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
